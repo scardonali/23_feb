42d95ccceb9c9dfb3ad19d896ecedeecf64ca2aa
scardonali <scardonali@unal.edu.co>
2026-02-23 16:26:54 -0500

    Cambios realizados

--- archivo.js
+++ archivo.js
@@ -1,4 +1,17 @@
-function prueba(){
-    alert("Hola desde JS");
+function suma(){
+    var a1 = document.getElementById("a1").value;
+    var a2 = document.getElementById("a2").value;
+    a1=parseInt(a1);
+    a2=parseInt(a2);
+    alert(a1+a2);
+
+}
+
+function restar(){
+    var x1= document.getElementById("r1")
+    var x2= document.getElementById("r2")
+    x1=parseInt(x1);
+    x2=parseInt(x2);
+    alert(x1-x2);
 
 }
