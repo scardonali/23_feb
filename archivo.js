ec7f6eb4bbbef96493eec353b903d2d85854e6a9
scardonali <scardonali@unal.edu.co>
2026-02-25 11:35:58 -0500

    Cambios hechos en clase

--- archivo.js
+++ archivo.js
@@ -1,33 +1,26 @@
 function suma(){
-    var a1 = document.getElementById("a1").value;
-    var a2 = document.getElementById("a2").value;
-    a1=parseInt(a1);
-    a2=parseInt(a2);
-    alert(a1+a2);
-
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    alert(num1 + num2);
 }
 
 function restar(){
-    var x1= document.getElementById("r1").value;
-    var x2= document.getElementById("r2").value;
-    x1=parseInt(x1);
-    x2=parseInt(x2);
-    alert(x1-x2);
-
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    alert(num1 - num2);
 }
 
 function multiplicar(){
-    var m1 = document.getElementById("m1").value;
-    var m2 = document.getElementById("m2").value;
-    m1=parseInt(m1);
-    m2=parseInt(m2);
-    alert(m1*m2);
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    alert(num1 * num2);
 }
 
 function division(){
-    var d1= document.getElementById("d1").value;
-    var d2= document.getElementById("d2").value;
-    d1=parseInt(d1)
-    d2=parseInt(d2)
-    alert(d1/d2)
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    alert(num1 / num2);
+    if (a2==0){
+        alert("El denominador debe ser 0")
+    }
 }
