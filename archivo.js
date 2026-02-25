977c453c2447ed8bf9206c488d9e2f5d678e1e38
scardonali <scardonali@unal.edu.co>
2026-02-25 12:30:59 -0500

    Se realizaron algunos cambios

--- archivo.js
+++ archivo.js
@@ -1,18 +1,23 @@
-
 function operacion(){
-    var num1= document.getElementById("num1").value;
-    var num2= document.getElementById("num2").value;
-    num1=parseFloat(num1)
-    num2=parseFloat(num2)
-    var select= document.getElementById("operacion").value;
-    if (select=="suma"){
-        alert(num1+num2)
-    }else if (select == "resta"){
-        alert(num1-num2)
 
-    }else if (select == "multiplicacion"){
-        alert(num1*num2)
-    }else if (select == "division"){
-        alert(num1/num2)
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var select = document.getElementById("operacion").value;
+
+    if (select == "suma"){
+        alert(num1 + num2);
+    }
+    else if (select == "resta"){
+        alert(num1 - num2);
+    }
+    else if (select == "multiplicacion"){
+        alert(num1 * num2);
+    }
+    else if (select == "division"){
+        if (num2 == 0){
+            alert("No se puede dividir entre 0");
+            return;
+        }
+        alert(num1 / num2);
     }
 }
